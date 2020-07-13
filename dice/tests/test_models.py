fbf9e25003a622f5bc7661b188b459dba31994c6
AlexHBall <timmygaines22@gmail.com>
2020-07-13 11:40:12 +0200

    remove reduntdant methods

--- dice/tests/test_models.py
+++ dice/tests/test_models.py
@@ -10,3 +10,10 @@ class ModelsTestCase(TestCase):
             dice_side_4="2", dice_side_5="3", dice_side_6="2")
         places = dice.get_places_dice()
         self.assertEqual(places, [1,0,0,2,3,2])
+
+    def test_places_dice_string(self):
+        dice = CharacterDice.objects.create(
+            character_name="Test Character", dice_side_1="1", dice_side_2="-1", dice_side_3="+4",
+            dice_side_4="2", dice_side_5="3", dice_side_6="2")
+        places = dice.get_probabilites()
+        self.assertEqual(places, "[1,0,0,2,3,2]")        
